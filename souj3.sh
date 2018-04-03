130129a232a3a9579dd7f57aa1dca2ad1560c48b
sowjanyadevdas7 <35593759+sowjanyadevdas7@users.noreply.github.com>
2018-04-03 14:55:08 +0530

    Add files via upload

---
+++ souj3.sh
@@ -0,0 +1,6 @@
+Employee Name    Employee ID    Designation             Salary
+roshni             110110         Manager               150000
+swathi             12390         Software Developer     50000
+sneha              125683        Network Engineer       45000
+aki                122787          M.D                  300000
+sahana             125376        C.E.0                  1500000
