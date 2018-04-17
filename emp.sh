9f8157144823acb0e1db3589d0ec1cd0d4ff24c8
sowjanyadevdas7 <35593759+sowjanyadevdas7@users.noreply.github.com>
2018-04-17 14:54:37 +0530

    Add files via upload

---
+++ emp.sh
@@ -0,0 +1,12 @@
+d	nmae 	salary 	designation
+1	sarah	13000	worker
+2	adma	13500	assistant
+3	paul	14000	chief_editor
+4	george	14500	managing_director
+5	sia	15000	manager
+6	nia	16000	clerk
+7	ron	16500	chairman
+8	taylor	14800	assistant_chief
+9	philip	15000	tema_head
+10	hannah	16900	chief_secretary
+
